2463c02298ca17ce64aaca27faadc73d0f7d534c
Emmanuel Jesus Estallo <erestallo@up.edu.ph>
2022-08-12 15:17:30 +0800

    charPyXyce

---
+++ xyceNet.sp
@@ -0,0 +1,18 @@
+* MOS Characterization
+
+* Include model files
+.include 22nm_cmos.pm
+.options TEMP = 27
+
+* Netlist parameters
+vd drain 0 dc 0.6
+vg gate 0 dc 0.6
+mn0 drain gate 0 0 n_22n l=100n w=1u
+
+
+.dc vg 0 1.4 0.001
+.op
+
+.print dc format=csv N(mn0:gm) N(mn0:vth) N(mn0:cgd) N(mn0:cgs) N(mn0:gds) i(vd) vg
+
+.end
---
+++ xyceOutputs.py
@@ -0,0 +1,67 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from math import pi as pi 
+fileName = input()
+data_ = pd.read_csv(fileName) 
+
+cols = ["N(MN0:GM)","N(MN0:VTH)","N(MN0:CGD)","N(MN0:CGS)","N(MN0:GDS)","I(VD)","VG"]
+
+gm = data_[[cols[0]]].to_numpy()
+vth = data_[[cols[1]]].to_numpy()
+cgd = -1*data_[[cols[2]]].to_numpy()
+cgs = -1*data_[[cols[3]]].to_numpy()
+gds = data_[[cols[4]]].to_numpy()
+id = -1*data_[[cols[5]]].to_numpy()
+vgs = data_[[cols[6]]].to_numpy() 
+
+cgg = cgs + cgd 
+
+vov = vgs - vth 
+gmro = np.divide(gm,gds)
+ft = np.divide(gm,2*pi*cgg)
+gmid = gm/id 
+ft_gmid = np.multiply(ft,gmid) 
+idw = 1e-6*id 
+
+
+fig,ax = plt.subplots(2,3)
+
+ax[0][0].plot(vgs,id)
+ax[0][0].set_title('vgs vs id')
+ax[0][0].set_xlabel('vgs')
+ax[0][0].set_ylabel('id')
+ax[0][0].grid()
+
+ax[0][1].plot(vgs,vov)
+ax[0][1].set_title('vgs vs vov')
+ax[0][1].set_xlabel('vgs')
+ax[0][1].set_ylabel('vov')
+ax[0][1].grid()
+
+ax[0][2].plot(vov,gmid)
+ax[0][2].set_title('vov vs gm/id')
+ax[0][2].set_xlabel('vov')
+ax[0][2].set_ylabel('gm/id')
+ax[0][2].grid()
+
+ax[1][0].plot(gmid,ft)
+ax[1][0].set_title('gm/id vs ft')
+ax[1][0].set_xlabel('gm/id')
+ax[1][0].set_ylabel('ft')
+ax[1][0].grid()
+
+ax[1][1].plot(gmid,gmro)
+ax[1][1].set_title('gm/id vs gm/gds')
+ax[1][1].set_xlabel('gm/id')
+ax[1][1].set_ylabel('gm/gds')
+ax[1][1].grid()
+
+ax[1][2].plot(gmid,ft_gmid)
+ax[1][2].set_title('gm/id vs ft*gm/id')
+ax[1][2].set_xlabel('gm/id')
+ax[1][2].set_ylabel('ft*gm/id')
+ax[1][2].grid()
+
+plt.tight_layout()
+plt.show()
