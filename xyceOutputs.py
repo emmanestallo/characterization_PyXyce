ad6ce9e33270f48c866b4c097e8abc47a731fff9
Emmanuel Jesus Estallo <erestallo@up.edu.ph>
2022-08-14 02:41:09 +0800

    final_

--- xyceOutputs.py
+++ xyceOutputs.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from math import pi as pi 
+from matplotlib.widgets import Cursor
 
 def getThresholdIndex(vgs,vth):
     i = 0 
@@ -43,37 +44,39 @@ def plot (fig,ax,lengthValue,initialLength=0):
     ax[0][0].set_title('vgs vs id')
     ax[0][0].set_xlabel('vgs')
     ax[0][0].set_ylabel('id')
-    ax[0][0].grid()
 
     ax[0][1].plot(vgs,vov,label=f'length={initialLength+lengthValue}nm')
     ax[0][1].set_title('vgs vs vov')
     ax[0][1].set_xlabel('vgs')
     ax[0][1].set_ylabel('vov')
-    ax[0][1].grid()
 
     ax[0][2].plot(vgs,gmid,label=f'length={initialLength+lengthValue}nm')
     ax[0][2].set_title('vgs vs gm/id')
     ax[0][2].set_xlabel('vgs')
     ax[0][2].set_ylabel('gm/id')
-    ax[0][2].grid()
 
     ax[1][0].plot(gmid,ft,label=f'length={initialLength+lengthValue}nm')
     ax[1][0].set_title('gm/id vs ft')
     ax[1][0].set_xlabel('gm/id')
     ax[1][0].set_ylabel('ft')
-    ax[1][0].grid()
 
     ax[1][1].plot(gmid,gmro,label=f'length={initialLength+lengthValue}nm')
     ax[1][1].set_title('gm/id vs gm/gds')
     ax[1][1].set_xlabel('gm/id')
     ax[1][1].set_ylabel('gm/gds')
-    ax[1][1].grid()
 
     ax[1][2].plot(gmid,idw,label=f'length={initialLength+lengthValue}nm')
     ax[1][2].set_title('gm/id vs id/w')
     ax[1][2].set_xlabel('gm/id')
     ax[1][2].set_ylabel('id/w')
-    ax[1][2].grid()
+
+def showGrid():
+    ax[0][0].grid(linestyle='--')
+    ax[0][1].grid(linestyle='--')
+    ax[0][2].grid(linestyle='--')
+    ax[1][0].grid(linestyle='--')
+    ax[1][1].grid(linestyle='--')
+    ax[1][2].grid(linestyle='--')
 
 data_ = pd.read_csv('xyceNet.sp.csv')
 nSweeps = int(input('Enter number of steps: '))
@@ -92,6 +95,22 @@ for i in range(nSweeps):
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = dict(zip(labels, handles))
 fig.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(0.01, 0.5), loc='center left')
+
+showGrid()
+
+cursor_00 = Cursor(ax[0][0], horizOn = True, vertOn=True, color='black', linewidth=1, 
+                useblit=True)
+cursor_01 = Cursor(ax[0][1], horizOn = True, vertOn=True, color='black', linewidth=1, 
+                useblit=True)                
+cursor_02 = Cursor(ax[0][2], horizOn = True, vertOn=True, color='black', linewidth=1, 
+                useblit=True)
+cursor_10 = Cursor(ax[1][0], horizOn = True, vertOn=True, color='black', linewidth=1, 
+                useblit=True)
+cursor_11 = Cursor(ax[1][1], horizOn = True, vertOn=True, color='black', linewidth=1, 
+                useblit=True)
+cursor_12 = Cursor(ax[1][2], horizOn = True, vertOn=True, color='black', linewidth=1, 
+                useblit=True)
+
 
 fig.tight_layout()
 plt.show()
